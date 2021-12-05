d13386b2381d7a0451af1ab6acd89dc67c8abc87
Sam Washington <sam@spwashi.com>
2021-12-05 15:22:00 -0600

    reformatted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
-import logo from './logo.svg';
 import './App.scss';
-import React, { useEffect, useCallback, useState, useMemo } from 'react'
+import React from 'react'
 
-  
-const opening = (<h1>Hello, visitor!</h1>);
+
+const opening      = (<h1>Hello, visitor!</h1>);
 const introduction = "My name is Jordan and welcome to my website! I  am a Front End Engineer with a background in PHP, MySQL, JavaScript, CSS, and HTML. Feel free to check out the options below:"
 
 
 function App() {
-  return (
-    <div className="App">
-      <div className="App-opener">
-      {opening}
-      <hr />
-      <p>{introduction}</p>
-      <hr />
-      </div>
-      <div className="Options">
-      <div className="Github"><p  title="Jordan's Github Profile"><a href="https://github.com/jaytoven527">Check out my Github!</a></p></div>
-      <div className="Linkedin"><p title="Jordan's Linkedin Profile"><a href="https://www.linkedin.com/in/jordan-norwood-a971b8186/">Hire me.</a></p></div>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="App-opener">
+                {opening}
+                <hr/>
+                <p>{introduction}</p>
+                <hr/>
+            </div>
+            <div className="Options">
+                <div className="Github">
+                    <p title="Jordan's Github Profile">
+                        <a href="https://github.com/jaytoven527">Check out my Github!</a>
+                    </p>
+                </div>
+                <div className="Linkedin">
+                    <p title="Jordan's Linkedin Profile">
+                        <a href="https://www.linkedin.com/in/jordan-norwood-a971b8186/">Hire me.</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
