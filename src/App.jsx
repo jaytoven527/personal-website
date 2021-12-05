d381639c64fb6ecf22a08f57e392c15a5e5e652b
Sam Washington <sam@spwashi.com>
2021-12-05 15:33:01 -0600

    changed attribute from title to aria-label

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,12 @@ function App() {
             </header>
             <div className="options">
                 <div className="github">
-                    <p title="Jordan's Github Profile">
+                    <p aria-label="Jordan's Github Profile">
                         <a href="https://github.com/jaytoven527">Check out my Github!</a>
                     </p>
                 </div>
                 <div className="linkedin">
-                    <p title="Jordan's Linkedin Profile">
+                    <p aria-label="Jordan's Linkedin Profile">
                         <a href="https://www.linkedin.com/in/jordan-norwood-a971b8186/">Hire me.</a>
                     </p>
                 </div>
